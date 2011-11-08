c86140cf7365c3267b7e9c00ff806adb7380e00f
William Blazer <will.blazer@gmail.com>
2011-11-08 14:54:40 -0500

    added a few methods to deck and got it started

--- src/edu/ncsu/csc216/solitaire/model/Deck.java
+++ src/edu/ncsu/csc216/solitaire/model/Deck.java
@@ -10,10 +10,28 @@ public class Deck {
 
 	private LinkedList<Integer> deck;
 	
-	public Deck(int ) {
+	public Deck(int[] deck ) {
+		// checks the int[] deck for the existance of 1-28 before
+		// building it into linked list
 		
+		boolean valueFound = false;
+		int j = 0;
+		
+		for (j = 1; j < 28; j++)
+			valueFound = false;
+			for (int i = 0; i < 27; i++) {
+				if (j == deck[i]) {
+					valueFound = true;
+				}
+			}
+			if (valueFound == false) {
+				throw new IllegalArgumentException("There deck is invalid, missing a number between 1 and 28");
+			}
 	}
 	
 	public int getKeySteamValue()  {
 		
+		
+		return 0;
 	}
+}
