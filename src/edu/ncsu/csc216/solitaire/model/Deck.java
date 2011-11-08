fa72afe951738f4106f8a18a480ebd993dc13b5e
William Blazer <will.blazer@gmail.com>
2011-11-08 15:11:44 -0500

    finished deck's constructor... i think

--- src/edu/ncsu/csc216/solitaire/model/Deck.java
+++ src/edu/ncsu/csc216/solitaire/model/Deck.java
@@ -8,25 +8,31 @@ import java.util.LinkedList;
  */
 public class Deck {
 
-	private LinkedList<Integer> deck;
+	private LinkedList<Integer> deck = new LinkedList<Integer>();
 	
-	public Deck(int[] deck ) {
+	public Deck(int[] deckArray ) {
 		// checks the int[] deck for the existance of 1-28 before
 		// building it into linked list
-		
 		boolean valueFound = false;
 		int j = 0;
 		
 		for (j = 1; j < 28; j++)
 			valueFound = false;
 			for (int i = 0; i < 27; i++) {
-				if (j == deck[i]) {
+				if (j == deckArray[i]) {
 					valueFound = true;
 				}
 			}
 			if (valueFound == false) {
 				throw new IllegalArgumentException("There deck is invalid, missing a number between 1 and 28");
 			}
+	
+	
+		// now the deckArray[] is turned into the actual linked list deck 
+	
+		for (int k = 0; k < 27; k++) {
+			deck.add(deckArray[k]);
+		}
 	}
 	
 	public int getKeySteamValue()  {
