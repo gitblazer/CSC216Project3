bdad8ce9953e730d49276b93e4d1bd06c26d07f9
Andrew Kofink <ajkofink@gmail.com>
2011-11-10 14:10:11 -0500

    made a new test package

---
+++ src/edu/ncsu/csc216/solitaire/test/UITest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.solitaire.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author akofink
+ *
+ */
+public class UITest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
