a91553447a03c5b67a9bef284da75ad91f8009a4
Andrew Kofink <ajkofink@gmail.com>
2011-11-10 22:43:00 -0500

    fixed deck check

--- JGraspProject3/edu/ncsu/csc216/solitaire/model/Deck.java
+++ JGraspProject3/edu/ncsu/csc216/solitaire/model/Deck.java
@@ -13,12 +13,13 @@ public class Deck {
 	private static final int JOKER2 = 28;
 
 	/**
-	 * 
+	 * The Deck ArrayList
 	 */
 	private LinkedList<Integer> deck = new LinkedList<Integer>();
 	
 	/**
-	 * @param deckArray
+	 * Creates the deck arrayList
+	 * @param deckArray Array with the deck values
 	 */
 	public Deck(int[] deckArray ) {
 		// checks the int[] deck for the existance of 1-28 before
@@ -26,17 +27,17 @@ public class Deck {
 		boolean valueFound = false;
 		int j = 0;
 		
-		for (j = 1; j < 28; j++)
+		for (j = 1; j < 28; j++) {
 			valueFound = false;
 			for (int i = 0; i < 27; i++) {
 				if (j == deckArray[i]) {
 					valueFound = true;
 				}
 			}
-			if (valueFound == false) {
+			if (!valueFound) {
 				throw new IllegalArgumentException("There deck is invalid, missing a number between 1 and 28");
 			}
-	
+		}
 	
 		// now the deckArray[] is turned into the actual linked list deck 
 	
@@ -46,9 +47,10 @@ public class Deck {
 	}
 	
 	/**
-	 * @return
+	 * returns the next keystream value
+	 * @return keyStream Valye
 	 */
-	public int getKeySteamValue()  {
+	public int getKeystreamValue()  {
 		
 		stepOne();
 		stepTwo();
@@ -58,34 +60,31 @@ public class Deck {
 	}
 	
 	/**
-	 * 
+	 * Step one
 	 */
 	private void stepOne() {
 		// find A Joker (value 27)
 		// swap it with the card in position below it
 		// ** if joker is position 28, then it circulates to position 1 **
 		int tempVal = 0;
-		//System.out.println("B joker position1: " + deck.indexOf(28));
-		if (deck.indexOf(27)+1 == 28) {
+		if (deck.indexOf(27) + 1 == 28) {
 			tempVal = deck.get(0);
 		}
 		else {
-			tempVal = deck.get(deck.indexOf(27)+1);
-		}
-		//System.out.println("B joker position2: " + deck.indexOf(28));
+			tempVal = deck.get(deck.indexOf(27) + 1);
+		}	
 		int jokerIndexA = deck.indexOf(27);
 		if (jokerIndexA + 1 == 28) {
-			deck.set(0,27);
+			deck.set(0 , 27);
 		}
 		else {
 			deck.set(jokerIndexA + 1,27);
-		}
-		//System.out.println("B joker position3: " + deck.indexOf(28));
-		deck.set(jokerIndexA,tempVal);
+		}		
+		deck.set(jokerIndexA , tempVal);
 	}
 	
 	/**
-	 * 
+	 * Step two
 	 */
 	private void stepTwo() {
 		// find B Joker (value 28)
@@ -101,14 +100,14 @@ public class Deck {
 			tempVal2 = deck.get(deck.indexOf(28) + 1);
 		}
 		
-		int jokerIndexB= deck.indexOf(28);
+		int jokerIndexB = deck.indexOf(28);
 		if  (jokerIndexB + 1 > 27) {
 			deck.set(jokerIndexB + 1 - 28,28);
 		}
 		else {
 			deck.set(jokerIndexB + 1,28);
 		}
-		deck.set(jokerIndexB,tempVal2);
+		deck.set(jokerIndexB , tempVal2);
 				
 		//swaps forward the first of two positions
 		tempVal2 = 0;
@@ -119,24 +118,23 @@ public class Deck {
 			tempVal2 = deck.get(deck.indexOf(28) + 1);
 		}
 		
-		jokerIndexB= deck.indexOf(28);
+		jokerIndexB = deck.indexOf(28);
 		if  (jokerIndexB + 1 > 27) {
 			deck.set(jokerIndexB + 1 - 28,28);
 		}
 		else {
 			deck.set(jokerIndexB + 1,28);
 		}
-		deck.set(jokerIndexB,tempVal2);
+		deck.set(jokerIndexB , tempVal2); 
 	}
 	
 	/**
-	 * 
+	 * Step Three
 	 */
 	private void stepThree() {
 		// swap the top third of the deck with the bottom third of the deck
 		// the two jokers denote the split points
-		printDeck(deck);
-		
+				
 		int cutJokerA = deck.indexOf(27);
 		int cutJokerB = deck.indexOf(28);
 		int bottomCutOff = deck.get(27);
@@ -151,8 +149,8 @@ public class Deck {
 			topJoker = JOKER2;
 			bottomJoker = JOKER1;
 		}
-		System.out.println(deck.indexOf(topJoker));
-		System.out.println(deck.indexOf(bottomJoker));
+		//System.out.println(deck.indexOf(topJoker));
+		//System.out.println(deck.indexOf(bottomJoker));
 		
 		int value = deck.get(0);
 		while (deck.indexOf(topJoker) != 0) {
@@ -161,64 +159,90 @@ public class Deck {
 			deck.remove(0);
 		}
 		
-		printDeck(deck);
-		
 		while (deck.indexOf(value) != deck.indexOf(bottomJoker) + 1) {
 			//move values between bottom joker and the decks original bottom to the top and delete
 			deck.addFirst(deck.get(deck.indexOf(value) - 1));
 			deck.remove(deck.indexOf(value) - 1);
-		}
-		
-		printDeck(deck);
+		}			
 	}
 	
 	/**
-	 * 
+	 * Step four
 	 */
 	private void stepFour() {
 		// get the value of the bottom card (position 27)
 		// move that number of cards from the top of the deck to the bottom
 		// replace the bottom card on the bottom again
 		// ** if bottom card value = 27 or 28 (a joker) then use 27 regardless *
+		
+		System.out.print("Before: ");
+				
 		int temp3 = deck.get(27);
+		deck.remove(27);
 		for (int i = 0; i < temp3; i++) {
 			deck.addLast(deck.get(0));
 			deck.remove(0);
 		}
-		deck.addLast(deck.indexOf(temp3));
-		deck.remove(deck.indexOf(temp3));
+		deck.addLast(temp3);
+				
+		System.out.print("After:  ");
+		
 	}
 	
 	/**
-	 * @return
+	 * fifth step
+	 * @return the value for keyValue
 	 */
 	private int stepFive() {
 		// ** read the top cards value (28 or 27 both are 27 again) **
 		// go down into the deck that many cards
 		// return the value of the next card
+		printDeck(deck);
+		
 		int temp4 = deck.get(0);
 		if (temp4 == 28) {
 			temp4 = 27;
 		}
 		
 		int returnMe = 0;
+		/*
 		if (temp4 + 1 > 27) {
 			returnMe = deck.get(temp4 + 1 - 27);
 		}
 		else {
 			returnMe = deck.get(temp4 + 1);
 		}
+		*/
+		
+		returnMe = deck.get(temp4);
 	
-		System.out.println("Keystream Value returned: " + returnMe);
+		//System.out.println("Keystream Value returned: " + returnMe);
 		//printDeck(deck);
 		if (returnMe == 28) {
 			returnMe = 27;
 		}
+		printDeck(deck);
+		System.out.println("Return value: " + returnMe);
+		
+		/*
+		int gr = '@' + 0;
+		System.out.println("value of @: " + gr);
+		int gr1 = 'Z' + 0;
+		System.out.println("value of Z: " + gr1);
+		int gr2 = 'z' + 0;
+		System.out.println("value of z: " + gr2);
+		int gr3 = 'a' + 0;
+		System.out.println("value of a: " + gr3);
+		int gr4 = 'A' + 0;
+		System.out.println("value of A: " + gr4);
+		*/
+		
 		return returnMe;
 	}
 	
 	/**
-	 * @param deck2
+	 * used to print the deck for debugging
+	 * @param deck2 deck to be printed
 	 */
 	public static void printDeck(LinkedList<Integer> deck2) { 
 		// below is a loop to print out the arraylist  for debugging purposes
--- JGraspProject3/edu/ncsu/csc216/solitaire/model/Message.java
+++ JGraspProject3/edu/ncsu/csc216/solitaire/model/Message.java
@@ -1,33 +1,57 @@
 package edu.ncsu.csc216.solitaire.model;
 
+/**
+ * The message class
+ * @author William Blazer + Andrew Kofink
+ *
+ */
 public class Message {
 	
+	/**
+	 * the message array
+	 */
 	private int[] message;
 	
+	/**
+	 * The message encryption method
+	 * @param messageString the message to be encrypted
+	 */
 	public Message(String messageString) {
 		//Replace all non-alphabetic characters with '' and spaces with '[' (The ASCII value of 'Z' + 1)
-		char[] characterArray = messageString.toUpperCase().replaceAll("[^A-Z ]+","").replaceAll("[ ]","[").toCharArray();
+		char[] characterArray = messageString.toUpperCase().replaceAll("[^A-Z ]+","").replaceAll("[ ]" , "[").toCharArray();
 		message = new int[characterArray.length];
 		for (int i = 0; i < characterArray.length; i++) {
 			message[i] = characterArray[i] - 'A' + 1;
 		}
 	}
-
+	// the @ sybol is value 64, right before 'A' value of 65, check boundary down here
+	// the characters for ' ' spaces are one more than they should be. I.E , a 'X' is being returned when it should be a 'W' for spaces
+	
+	/**
+	 * The encryption method
+	 * @param deck the deck to be processed
+	 * @return the encrypted character
+	 */
 	public String encrypt(Deck deck) {
 		System.out.println("Encrypting...");
 		char[] characterArray = new char[message.length];
 		for (int i = 0; i < message.length; i++) {
-			message[i] = (message[i] + deck.getKeySteamValue()) % ('[' - 'A');
+			message[i] = (message[i] + deck.getKeystreamValue()) % ('[' - 'A');
 			characterArray[i] = (char)(message[i] + 'A' - 1);
 		}
 		return String.copyValueOf(characterArray).replaceAll("\\[", " ");
 	}
 
+	/**
+	 * The decryption Method
+	 * @param deck the deck to be processed 
+	 * @return a decrypted character
+	 */
 	public String decrypt(Deck deck) {
 		System.out.println("Decrypting...");
 		char[] characterArray = new char[message.length];
 		for (int i = 0; i < message.length; i++) {
-			message[i] = (message[i] - deck.getKeySteamValue());
+			message[i] = (message[i] - deck.getKeystreamValue());
 			if (message[i] <= 0) {
 				message[i] += '[' - 'A';
 			}
