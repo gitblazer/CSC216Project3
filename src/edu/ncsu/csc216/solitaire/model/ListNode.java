ed81e36f1dcd89f2cc007f5e1cca912ecfd6cb05
William Blazer <will.blazer@gmail.com>
2011-11-12 14:22:29 -0500

    Added DeckLinkedList and ListNode classes and thier skeletons

---
+++ src/edu/ncsu/csc216/solitaire/model/ListNode.java
@@ -0,0 +1,19 @@
+package edu.ncsu.csc216.solitaire.model;
+
+public class ListNode {
+
+	int data;
+	ListNode next;
+	
+	public ListNode() {
+		
+	}
+	
+	public ListNode(int data) {
+		
+	}
+	
+	public ListNode(int data, ListNode next) {
+		
+	}
+}
