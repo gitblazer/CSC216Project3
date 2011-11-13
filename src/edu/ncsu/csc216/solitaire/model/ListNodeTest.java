5159c1b5469c8ac4552b447ab632c4e56aaf2d99
Andrew Kofink <ajkofink@gmail.com>
2011-11-13 14:55:15 -0500

    ListNodeTest done

--- src/edu/ncsu/csc216/solitaire/model/ListNodeTest.java
+++ src/edu/ncsu/csc216/solitaire/model/ListNodeTest.java
@@ -2,11 +2,23 @@ package edu.ncsu.csc216.solitaire.model;
 
 import junit.framework.TestCase;
 
+/**
+ * Tests the ListNode class
+ * @author William Blazer, Andrew Kofink
+ */
 public class ListNodeTest extends TestCase {
 
+	/**
+	 * Tests the node object
+	 */
 	public void testListNodeIntListNode() {
 		ListNode testNode = new ListNode();
 		assertEquals(testNode.next, null);
+		testNode = new ListNode(5);
+		assertEquals(testNode.data, 5);
+		ListNode testNode2 = new ListNode(5, null);
+		testNode = new ListNode(5, testNode2);
+		assertEquals(testNode.next, testNode2);
 	}
 
 }
