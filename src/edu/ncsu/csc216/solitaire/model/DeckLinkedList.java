71f0a65d13c2824de7e456291bf0e5683b19d2c0
Andrew Kofink <ajkofink@gmail.com>
2011-11-13 13:57:15 -0500

    finished down to add [recursive]

--- src/edu/ncsu/csc216/solitaire/model/DeckLinkedList.java
+++ src/edu/ncsu/csc216/solitaire/model/DeckLinkedList.java
@@ -1,63 +1,128 @@
 package edu.ncsu.csc216.solitaire.model;
 
+/**
+ * The LinkedList for the Deck of cards
+ * @author William Blazer, Andrew Kofink
+ */
 public class DeckLinkedList {
 	
+	/**
+	 * The front node of the list
+	 */
 	private ListNode front;
 	
+	/**
+	 * The LinkedList for the Deck of cards
+	 */
 	public DeckLinkedList() {
-		
+		front = null;
 	}
 	
+	/**
+	 * Adds a value to the end of the LinkedList
+	 * @param value The value to add
+	 */
 	public void add(int value) {
-		
+		add(value, front);
 	}
 	
+	/**
+	 * A helper method for add(int value)
+	 * @param value The value to be added
+	 * @param index The current parsed index
+	 */
+	private void add(int value, ListNode current) {
+		if (current == null) {
+			current = new ListNode(0);
+		} else {
+			add(value, current.next);
+		}
+	}
+	
+	/**
+	 * @param value
+	 */
 	public void addFirst(int value) {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void clear() {
 		
 	}
 	
+	/**
+	 * @param list
+	 */
 	public void concat(DeckLinkedList list) {
 		
 	}
 	
+	/**
+	 * @param index
+	 * @return
+	 */
 	public DeckLinkedList detachAt(int index) {
 		
 		return null;
 	}
 	
+	/**
+	 * @param index1
+	 * @param index2
+	 */
 	public void exchange(int index1, int index2) {
 		
 	}
 	
+	/**
+	 * @param index
+	 * @return
+	 */
 	public int get(int index) {
 		
 		return -1;
 	}
 	
+	/**
+	 * @param value
+	 * @return
+	 */
 	public int indexOf(int value) {
 		
 		return -1;
 	}
 	
+	/**
+	 * @param index
+	 * @return
+	 */
 	public int remove(int index) {
 		
 		return -1;
 	}
 	
+	/**
+	 * @param index
+	 * @param value
+	 */
 	public void set(int index, int value) {
 		
 	}
 	
+	/**
+	 * @param value
+	 */
 	public void addLast(int value) {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void removeLast() {
 		
 	}
-	
 }
--- src/edu/ncsu/csc216/solitaire/model/ListNode.java
+++ src/edu/ncsu/csc216/solitaire/model/ListNode.java
@@ -1,19 +1,43 @@
 package edu.ncsu.csc216.solitaire.model;
 
+/**
+ * A list node contained in a linked list
+ * @author William Blazer, Andrew Kofink
+ */
 public class ListNode {
 
+	/**
+	 * The data contained in this node
+	 */
 	int data;
+	
+	/**
+	 * The following node
+	 */
 	ListNode next;
 	
+	/**
+	 * A node in a LinkedList
+	 */
 	public ListNode() {
-		
+		this(0);
 	}
 	
+	/**
+	 * A node in a LinkedList
+	 * @param data The data held in the node
+	 */
 	public ListNode(int data) {
-		
+		this(data, null);
 	}
 	
+	/**
+	 * A node in a LinkedList
+	 * @param data The data held in the node
+	 * @param next The following node
+	 */
 	public ListNode(int data, ListNode next) {
-		
+		this.data = data;
+		this.next = next;
 	}
 }
