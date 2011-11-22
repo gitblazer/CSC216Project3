2e5ad9507fd005582dae407f42caef892bc53463
Andrew Kofink <ajkofink@gmail.com>
2011-11-22 15:00:18 -0500

    added card images and CardIcons class

---
+++ src/edu/ncsu/csc216/solitaire/ui/CardIcons.java
@@ -0,0 +1,21 @@
+package edu.ncsu.csc216.solitaire.ui;
+
+import javax.swing.ImageIcon;
+
+import edu.ncsu.csc216.solitaire.model.Deck;
+
+public class CardIcons {
+	
+	/**
+	 * The icons of the deck
+	 */
+	private ImageIcon icons[];
+	
+	public void initIcons() {
+		
+	}
+	
+	public void displayDeck(Deck d) {
+		
+	}
+}
